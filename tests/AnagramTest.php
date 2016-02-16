d9a5a078a26152e842c7663ce045357ad94bd275
Jason Awbrey and Mary Warrington <jason.s.awbrey@gmail.com>
2016-02-16 11:31:04 -0800

    pass spec to formatList with multiple anagrams

--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -28,6 +28,18 @@
 			//Assert
 			$this->assertEquals(array('cheater' => 'aceehrt'), $result);
 		}
+
+		function test_formatList_multipleWords()
+		{
+			//Arrange
+			$test_Anagram = new Anagram("teacher", array('cheater' => 'cheater', 'hectare' => 'hectare', 'recheat' => 'recheat'));
+
+			//Act
+			$result = $test_Anagram->formatList($test_Anagram->getAnagrams());
+
+			//Assert
+			$this->assertEquals(array('cheater' => 'aceehrt', 'hectare' => 'aceehrt', 'recheat' => 'aceehrt'), $result);
+		}
 	}
 
 ?>
