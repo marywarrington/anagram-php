ffa01cd87672b5a992f322250e7c40c6a1017828
Jason Awbrey and Mary Warrington <jason.s.awbrey@gmail.com>
2016-02-16 14:21:19 -0800

    Add first working version

--- app/app.php
+++ app/app.php
@@ -1,19 +1,31 @@
 <?php
 
 	require_once __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__.'/../src/Anagram.php';
 
 	$app = new Silex\Application();
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
 	$app->get('/', function() use ($app){
-		// Possible code to use for instantiating a new Anagram object
-		// $anagrams = $_POST['anagrams'];
-		// $stripped = strip out commas
-		// $split = split string into an array, each element is it's own word
-		//
-		// $new_anagram = new Anagram($_POST['word'], $split)
-	
+		return $app['twig']->render('anagram.html.twig');
+	});
+
+	$app->post('/results', function() use ($app){
+		$grams = array();
+
+		$i = 1;
+		while ($i <= 3){
+			$grams[$_POST['anagram'.$i]] = $_POST['anagram'.$i];
+			$i++;
+		}
+
+		$anagram = new Anagram($_POST['word'], $grams);
+
+		$matches = array_keys($anagram->checkList());
+		$fails = array_keys($anagram->getFailedItems());
+
+		return $app['twig']->render('anagram.html.twig', array('matches' => $matches,'fails' => $fails));
 	});
 
 	return $app;
